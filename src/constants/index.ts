50d86a2b38993786a76eb7eacbdeed2c6cc4b6a0
Your Name <you@example.com>
2023-04-02 15:01:44 +0200

    test commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,18 +3,25 @@ import { ChainId, ETHER, JSBI, Percent, Token } from '@uniswap/sdk'
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
 export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+//  export const ROUTER_ADDRESS = '0x8210ec0ce16399f2d9a06a37c0708cd09c74d7a1'
 
 // a list of tokens by chain
 type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
-export const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin')
-export const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD//C')
-export const USDT = new Token(ChainId.MAINNET, '0xdAC17F958D2ee523a2206206994597C13D831ec7', 6, 'USDT', 'Tether USD')
-export const COMP = new Token(ChainId.MAINNET, '0xc00e94Cb662C3520282E6f5717214004A7f26888', 18, 'COMP', 'Compound')
-export const MKR = new Token(ChainId.MAINNET, '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18, 'MKR', 'Maker')
-export const CHI = new Token(ChainId.MAINNET, '0x0000000000004946c0e9F43F4Dee607b0eF1fA1c', 0, 'CHI', 'Chi Gastoken by 1inch')
+// export const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin')
+// export const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD//C')
+// export const USDT = new Token(ChainId.MAINNET, '0xdAC17F958D2ee523a2206206994597C13D831ec7', 6, 'USDT', 'Tether USD')
+// export const COMP = new Token(ChainId.MAINNET, '0xc00e94Cb662C3520282E6f5717214004A7f26888', 18, 'COMP', 'Compound')
+// export const MKR = new Token(ChainId.MAINNET, '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18, 'MKR', 'Maker')
+// export const CHI = new Token(ChainId.MAINNET, '0x0000000000004946c0e9F43F4Dee607b0eF1fA1c', 0, 'CHI', 'Chi Gastoken by 1inch')
+export const DAI = new Token(ChainId.GÖRLI, '0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844', 18, 'DAI', 'Dai Stablecoin')
+export const USDC = new Token(ChainId.GÖRLI, '0x07865c6e87b9f70255377e024ace6630c1eaa37f', 6, 'USDC', 'USD//C')
+export const USDT = new Token(ChainId.GÖRLI, '0xe802376580c10fe23f027e1e19ed9d54d4c9311e', 6, 'USDT', 'Tether USD')
+export const COMP = new Token(ChainId.GÖRLI, '0xcc7bb2d219a0fc08033e130629c2b854b7ba9195', 18, 'ZETA', 'Zeta')
+export const MKR = new Token(ChainId.GÖRLI, '0x2166903c38b4883b855ea2c77a02430a27cdfede', 18, 'ERC20', 'TestnetERC20')
+export const CHI = new Token(ChainId.GÖRLI, '0x0000000000004946c0e9F43F4Dee607b0eF1fA1c', 0, 'NUSD', 'NUSDToken')
 
 const ETH_ONLY: ChainTokenList = {
   [ChainId.MAINNET]: [ETHER],
@@ -27,27 +34,27 @@ const ETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...ETH_ONLY,
-  [ChainId.MAINNET]: [DAI, USDC, USDT, COMP, MKR, CHI]
+  [ChainId.GÖRLI]: [DAI, USDC, USDT, COMP, MKR, CHI]
 }
 
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...ETH_ONLY,
-  [ChainId.MAINNET]: [DAI, USDC, USDT, CHI]
+  [ChainId.GÖRLI]: [DAI, USDC, USDT, CHI]
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   ...ETH_ONLY,
-  [ChainId.MAINNET]: [ETHER, DAI, USDC, USDT, CHI]
+  [ChainId.GÖRLI]: [ETHER, DAI, USDC, USDT, CHI]
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
-  [ChainId.MAINNET]: [
-    [
-      new Token(ChainId.MAINNET, '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643', 8, 'cDAI', 'Compound Dai'),
-      new Token(ChainId.MAINNET, '0x39AA39c021dfbaE8faC545936693aC917d5E7563', 8, 'cUSDC', 'Compound USD Coin')
-    ],
+  [ChainId.GÖRLI]: [
+    // [
+    //   new Token(ChainId.MAINNET, '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643', 8, 'cDAI', 'Compound Dai'),
+    //   new Token(ChainId.MAINNET, '0x39AA39c021dfbaE8faC545936693aC917d5E7563', 8, 'cUSDC', 'Compound USD Coin')
+    // ],
     [USDC, USDT],
     [DAI, USDT],
     [ETHER, CHI],
